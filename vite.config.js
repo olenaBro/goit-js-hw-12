161e9e9387aaedb69517f0a68d21963a747ad332
Olena <olenkazaria@gmail.com>
2026-01-22 21:15:31 +0100

    fix base path to /goit-js-hw-12/

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/goit-js-hw-12/',
     root: 'src',
     build: {
       sourcemap: true,
